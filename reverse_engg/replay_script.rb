4d7ab0d739cd586d2fb94f02715646c536cac7a9
amdugar <amit@browserstack.com>
2014-04-29 12:45:34 +0530

    replay script read hub address and some caps changes

--- reverse_engg/replay_script.rb
+++ reverse_engg/replay_script.rb
@@ -11,13 +11,14 @@ require 'pry'
 @user_define = ARGV[4].to_i || 0
 @test_logs = ""
 @capabilities = true
+@hub = ENV["HUB"] || "hub.browserstack.com"
 
 if ARGV.size < 4
-  puts "Usage ruby replay_script.rb <username> <key> <session_id> <user_id> <user_define 0 || 1> <browser> <browser_version> <os> <os_version>" 
+  puts "Usage HUB=<hub> ruby replay_script.rb <username> <key> <session_id> <user_id> <user_define 0 || 1 || 2> <browser || browserName> <browser_version || version> <os || platform> <os_version>" 
   exit 
 end
 if @user_define == 1 && ARGV.size < 9
-  puts "Usage ruby replay_script.rb <username> <key> <session_id> <user_id> <user_define 0 || 1> <browser> <browser_version> <os> <os_version>" 
+  puts "Usage HUB=<hub> ruby replay_script.rb <username> <key> <session_id> <user_id> <user_define 0 || 1 || 2> <browser || browserName> <browser_version || version> <os || platform> <os_version>" 
 end
 
 # Input capabilities
@@ -29,14 +30,14 @@ def create_driver
   caps["os"] = @os
   caps["os_version"] = @os_version
   caps["browserName"] = @browserName
-  caps["platform"] = @platform
-  caps["version"] = @version
+  caps[:platform] = @platform
+  caps[:version] = @version
 
   caps["browserstack.debug"] = "true"
   caps[:name] = "Replay Script for #{@user_id} #{@session_id}"
   caps[:build] = "Replay Scripts"
   driver = Selenium::WebDriver.for(:remote,
-                                   :url => "http://#{@username}:#{@key}@hub.browserstack.com/wd/hub",
+                                   :url => "http://#{@username}:#{@key}@#{@hub}/wd/hub",
                                    :desired_capabilities => caps)
   @my_session_id = driver.instance_variable_get("@bridge").instance_variable_get("@session_id")
 end
@@ -59,23 +60,36 @@ def modify_capabilities(json)
     @version = ""
     @platform = ""
 
-    mod_json["desiredCapabilities"]["browserName"] = ""
-    mod_json["desiredCapabilities"]["version"] = ""
-    mod_json["desiredCapabilities"]["platform"] = ""
+    mod_json["desiredCapabilities"][:browserName] = ""
+    mod_json["desiredCapabilities"][:version] = ""
+    mod_json["desiredCapabilities"][:platform] = ""
 
     mod_json["desiredCapabilities"]["browser"] = @browser
     mod_json["desiredCapabilities"]["browser_version"] = @browser_version
     mod_json["desiredCapabilities"]["os"] = @os
     mod_json["desiredCapabilities"]["os_version"] = @os_version
-  else 
-    @browserName = mod_json["desiredCapabilities"]["browserName"]  || ""
-    @version = mod_json["desiredCapabilities"]["version"] || ""
-    @platform = mod_json["desiredCapabilities"]["platform"] || ""
+  elsif (@user_define == 0)
+    @browserName = mod_json["desiredCapabilities"][:browserName]  || ""
+    @version = mod_json["desiredCapabilities"][:version] || ""
+    @platform = mod_json["desiredCapabilities"][:platform] || ""
 
     @browser = mod_json["desiredCapabilities"]["browser"] || ""
     @browser_version = mod_json["desiredCapabilities"]["browser_version"] || ""
     @os = mod_json["desiredCapabilities"]["os"] || ""
     @os_version = mod_json["desiredCapabilities"]["os_version"] || ""
+  elsif (@user_define ==2)
+    @browserName = ARGV[5]
+    @version = ARGV[6]
+    @platform = ARGV[7]
+
+    mod_json["desiredCapabilities"][:browserName] = @browserName
+    mod_json["desiredCapabilities"][:version] = @version
+    mod_json["desiredCapabilities"][:platform] = @platform
+
+    mod_json["desiredCapabilities"]["browser"] = ""
+    mod_json["desiredCapabilities"]["browser_version"] = ""
+    mod_json["desiredCapabilities"]["os"] = ""
+    mod_json["desiredCapabilities"]["os_version"] = ""
   end
   return mod_json.to_json
 end
@@ -116,8 +130,8 @@ def simulate_test
       i+=1
     end
     puts ("\n##########################################################\n")
-    puts("REQUEST: \n curl -L --max-redirs 20 -v -i -H \'Accept: application/json\' -X #{split[0]} http://#{@username}:#{@key}@hub.browserstack.com/wd/hub#{split[1]} -d \'#{json.strip}\'") 
-    response = `curl -L --max-redirs 20 -v -i -H \'Accept: application/json\' -X #{split[0]} http://#{@username}:#{@key}@hub.browserstack.com/wd/hub#{split[1]} -d \'#{json.strip}\'`
+    puts("REQUEST: \n curl -L --max-redirs 20 -v -i -H \'Accept: application/json\' -X #{split[0]} http://#{@username}:#{@key}@#{@hub}/wd/hub#{split[1]} -d \'#{json.strip}\'") 
+    response = `curl -L --max-redirs 20 -v -i -H \'Accept: application/json\' -X #{split[0]} http://#{@username}:#{@key}@#{@hub}/wd/hub#{split[1]} -d \'#{json.strip}\'`
     puts ("\n----------------------------------------------------------\n")
     puts ("RESPONSE \n #{response}")
   end
