9b74f01284b8213cd8bccb73223a2148fd4fcb74
amdugar <amit@browserstack.com>
2014-04-30 14:40:36 +0530

    some enhancements

--- reverse_engg/replay_script.rb
+++ reverse_engg/replay_script.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'json'
 require 'selenium-webdriver'
+require 'colorize'
 require 'pry'
 
 ## Variables
@@ -22,20 +23,35 @@ if @user_define == 1 && ARGV.size < 9
 end
 
 # Input capabilities
-
+def set_chrome_profile(caps)
+  caps["chromeOptions"] = {}
+  caps["chromeOptions"]["binary"] = ENV["CBINARY"]
+end
+def set_firefox_profile(caps)
+  caps["firefox_binary"] = ENV["FBINARY"]
+end
 def create_driver
   caps = Selenium::WebDriver::Remote::Capabilities.new
+  caps[:browserName] = @browserName
+  caps["browserName"] = @browserName
+  caps["platform"] = @platform
+  caps[:platform] = @platform
+  caps["version"] = @version
+  caps[:version] = @version
   caps["browser"] = @browser
   caps["browser_version"] = @browser_version
   caps["os"] = @os
   caps["os_version"] = @os_version
-  caps["browserName"] = @browserName
-  caps[:platform] = @platform
-  caps[:version] = @version
 
   caps["browserstack.debug"] = "true"
   caps[:name] = "Replay Script for #{@user_id} #{@session_id}"
   caps[:build] = "Replay Scripts"
+  if ENV["CBINARY"] != nil
+    set_chrome_profile(caps)
+  end
+  if ENV["FBINARY"] != nil
+    set_firefox_profile(caps)
+  end
   driver = Selenium::WebDriver.for(:remote,
                                    :url => "http://#{@username}:#{@key}@#{@hub}/wd/hub",
                                    :desired_capabilities => caps)
@@ -51,6 +67,9 @@ end
 def modify_capabilities(json)
   @capabilities = false
   mod_json = JSON.parse(json.strip)
+  mod_json["desiredCapabilities"][:name] = "Replay Script for #{@user_id} #{@session_id}"
+  mod_json["desiredCapabilities"][:build] = "Replay Scripts"
+  mod_json["desiredCapabilities"][:project] = ""
   if (@user_define == 1)
     @browser = ARGV[5] || ""
     @browser_version = ARGV[6] || ""
@@ -69,9 +88,9 @@ def modify_capabilities(json)
     mod_json["desiredCapabilities"]["os"] = @os
     mod_json["desiredCapabilities"]["os_version"] = @os_version
   elsif (@user_define == 0)
-    @browserName = mod_json["desiredCapabilities"][:browserName]  || ""
-    @version = mod_json["desiredCapabilities"][:version] || ""
-    @platform = mod_json["desiredCapabilities"][:platform] || ""
+    @browserName = mod_json["desiredCapabilities"]["browserName"]  || ""
+    @version = mod_json["desiredCapabilities"]["version"] || ""
+    @platform = mod_json["desiredCapabilities"]["platform"] || ""
 
     @browser = mod_json["desiredCapabilities"]["browser"] || ""
     @browser_version = mod_json["desiredCapabilities"]["browser_version"] || ""
@@ -81,10 +100,12 @@ def modify_capabilities(json)
     @browserName = ARGV[5]
     @version = ARGV[6]
     @platform = ARGV[7]
-
+puts @browserName
+puts @version
+puts @platform
     mod_json["desiredCapabilities"][:browserName] = @browserName
     mod_json["desiredCapabilities"][:version] = @version
-    mod_json["desiredCapabilities"][:platform] = @platform
+    mod_json["desiredCapabilities"]["platform"] = @platform
 
     mod_json["desiredCapabilities"]["browser"] = ""
     mod_json["desiredCapabilities"]["browser_version"] = ""
@@ -112,15 +133,23 @@ def get_logs
     if (@capabilities)
       json = modify_capabilities(json)
     end
+    json.gsub!("'", "")
     @test_logs << "#{request} #{json.strip}\n"
   end
   @test_logs.strip!.gsub!("\n \n", "\n")
 end
 
 def simulate_test
+  puts "Starting test for #{@session_id} with user_id #{@user_id} and replay_id #{@my_session_id}"
   @test_logs << "\nDELETE /session/#{@session_id}"
   start_time = Time.now
+  a = true
   @test_logs.each do |line|
+    next if line.strip.empty?
+    if a
+      a = false
+      next
+    end
     line.sub!(@session_id, @my_session_id)
     split = line.split(' ')
     i = 2
@@ -130,18 +159,18 @@ def simulate_test
       i+=1
     end
     puts ("\n##########################################################\n")
-    puts("REQUEST: \n curl -L --max-redirs 20 -v -i -H \'Accept: application/json\' -X #{split[0]} http://#{@username}:#{@key}@#{@hub}/wd/hub#{split[1]} -d \'#{json.strip}\'") 
-    response = `curl -L --max-redirs 20 -v -i -H \'Accept: application/json\' -X #{split[0]} http://#{@username}:#{@key}@#{@hub}/wd/hub#{split[1]} -d \'#{json.strip}\'`
+    puts("REQUEST: \n curl -L --max-redirs 20 -v -i -H \'Accept: application/json\' -X #{split[0]} http://#{@username}:#{@key}@#{@hub}/wd/hub#{split[1]} -d \'#{json.strip}\'".blue) 
+    response = `curl -s -L --max-redirs 20 -i -H \'Accept: application/json\' -X #{split[0]} http://#{@username}:#{@key}@#{@hub}/wd/hub#{split[1]} -d \'#{json.strip}\'`
     puts ("\n----------------------------------------------------------\n")
-    puts ("RESPONSE \n #{response}")
+    puts ("RESPONSE \n #{response}".green)
   end
   end_time = Time.now 
   @replay_time = (end_time-start_time).to_i
 end
 def print_times
-  puts ("\n##########################################################\n")
-  puts "Client Time = #{@client_time} secs and Replay Time = #{@replay_time} secs"
-  puts ("##########################################################\n")
+  puts ("\n##########################################################".red)
+  puts "#{@my_session_id} #{@session_id} Client Time = #{@client_time} secs and Replay Time = #{@replay_time} secs".red
+  puts ("##########################################################\n".red)
 end
 get_logs
 create_driver
